6a5d97b5e74de8c2f0201115be31129bf1a4bcdd
draakan <draakan79@gmail.com>
2024-07-06 12:45:08 +0200

    initial commit

---
+++ apps/notifications/src/notifications.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { NotifyEmailDto } from './dto/notify-email.dto';
+import { createTransport } from 'nodemailer';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class NotificationsService {
+  private readonly transporter = createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: this.configService.get('SMTP_USER'),
+      clientId: this.configService.get('GOOGLE_OAUTH_CLIENT_ID'),
+      clientSecret: this.configService.get('GOOGLE_OAUTH_CLIENT_SECRET'),
+      refreshToken: this.configService.get('GOOGLE_OAUTH_REFRESH_TOKEN'),
+    },
+  });
+
+  constructor(private readonly configService: ConfigService) {}
+
+  async notifyEmail({ email }: NotifyEmailDto) {
+    await this.transporter.sendMail({
+      from: this.configService.get('SMTP_USER'),
+      to: email,
+      subject: 'Nest microservices',
+      text: 'It works!',
+    });
+  }
+}
---
+++ apps/payments/src/payments.service.ts
@@ -0,0 +1,39 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
+import { NOTIFICATIONS_SERVICE } from '@app/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { PaymentsCreateCharge } from './dto/payments-create-charge.dto';
+
+@Injectable()
+export class PaymentsService {
+  private readonly stripe = new Stripe(
+    this.configService.get('STRIPE_SECRET_KEY'),
+    {
+      apiVersion: '2024-04-10',
+    },
+  );
+
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE)
+    private readonly notificationsService: ClientProxy,
+  ) {}
+
+  async createCharge({ amount, email }: PaymentsCreateCharge) {
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      payment_method: 'pm_card_visa',
+      amount: amount * 100,
+      confirm: true,
+      automatic_payment_methods: {
+        enabled: true,
+        allow_redirects: 'never',
+      },
+      currency: 'usd',
+    });
+
+    this.notificationsService.emit('notify_email', { email });
+
+    return paymentIntent;
+  }
+}
